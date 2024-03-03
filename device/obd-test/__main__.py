3b57715418e44252b139dd22d31719cec2ef1364
ahnsv <ahnsv@bc.edu>
2024-03-03 22:36:26 +0900

    feat(device): add obd test

---
+++ device/obd-test/__main__.py
@@ -0,0 +1,12 @@
+import obd
+
+def main():
+    connection = obd.OBD() # auto-connects to USB or RF port
+    cmd = obd.commands.SPEED # select an OBD command (sensor)
+    response = connection.query(cmd) # send the command, and parse the response
+
+    print(response.value) # returns unit-bearing values thanks to Pint
+    print(response.value.to("mph")) # user-friendly unit conversions
+
+if __name__ == "__main__":
+    main()
